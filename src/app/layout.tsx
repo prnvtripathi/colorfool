a2d357a5e9fcfc681bee2a95b57460d0874bb1f2
Pranav Tripathi <111558974+prnvtripathi@users.noreply.github.com>
2025-08-18 17:52:01 +0530

    feat: add @vercel/analytics dependency and integrate Analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { ThemeProvider } from '@/components/ThemeProvider';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'ColorFool',
@@ -23,6 +24,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
